8083600763e1ee7d426b909d696c1a3cdc5e1749
alexsilva <geniofuturo@gmail.com>
2024-05-28 14:24:18 -0300

    Add basic setup.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+	name='xadmin-notification',
+	version='1.0.0',
+	packages=[
+		'xplugin_notification',
+		'xplugin_notification.rest',
+		'xplugin_notification.migrations'
+	],
+	url='https://github.com/alexsilva/xadmin-content-url',
+	license='MIT',
+	author='Alex',
+	author_email='',
+	description='Notification system for administration users.'
+)
