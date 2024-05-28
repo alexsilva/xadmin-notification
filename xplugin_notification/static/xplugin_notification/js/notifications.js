6c909e9232f65989fbcdc06690e07b2602bbe42b
alexsilva <geniofuturo@gmail.com>
2024-05-28 20:09:17 -0300

    Lazy notification load.

--- xplugin_notification/static/xplugin_notification/js/notifications.js
+++ xplugin_notification/static/xplugin_notification/js/notifications.js
@@ -1,14 +1,16 @@
 $(function () {
-    var notifications = $(".dropdown-menu .notification-message-item");
-    $.ajax({
-        url: notifications.data("list_url"),
-        data: {"plugin": "xnotification"},
-    }).done(function (data) {
-        $.each(data, function (index, notification) {
-            var message = $("#notification_message").template_render$({
-                notification: notification
+    $(".nav-item .notification-message").click(function () {
+        var notifications = $(".dropdown-menu .notification-message-item");
+        $.ajax({
+            url: notifications.data("list_url"),
+            data: {"plugin": "xnotification"},
+        }).done(function (data) {
+            $.each(data, function (index, notification) {
+                var message = $("#notification_message").template_render$({
+                    notification: notification
+                });
+                notifications.append(message);
             });
-            notifications.append(message);
-        });
+        })
     })
 })
