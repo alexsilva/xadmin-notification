a17d4848ddd5d9e51c9763747031172324a569f0
alexsilva <geniofuturo@gmail.com>
2024-05-28 14:38:23 -0300

    Registration of the notification model.

--- xplugin_notification/adminx.py
+++ xplugin_notification/adminx.py
@@ -1,6 +1,13 @@
 from xadmin.sites import site
 from xadmin.views import ModelAdminView
+import xadmin.sites
 
+from xplugin_notification.models import Notification
 from xplugin_notification.plugin import NotificationAdminPlugin
 
 site.register_plugin(NotificationAdminPlugin, ModelAdminView)
+
+
+@xadmin.sites.register(Notification)
+class NotificationAdmin:
+	...
