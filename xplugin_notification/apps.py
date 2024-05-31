e245eb029b3f2472b74bacdd116e4fada181866b
alexsilva <geniofuturo@gmail.com>
2024-05-31 10:22:54 -0300

    Update translatation to app verbose_name.

--- xplugin_notification/apps.py
+++ xplugin_notification/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext as _
 
 
 class XpluginNotificationConfig(AppConfig):
 	name = 'xplugin_notification'
-	verbose_name = 'Notificação do admin'
+	verbose_name = _('Notifications (admin)')
 
 	def ready(self):
 		...
