4b3fb9dd1a4874decc8ac98c3714977a8b254d75
alexsilva <geniofuturo@gmail.com>
2024-08-09 12:59:05 -0300

    Implemented view to mark notifications as read.

---
+++ xplugin_notification/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import get_object_or_404, redirect
+from django.utils import timezone
+from xadmin.views import BaseAdminView
+
+from xplugin_notification.models import Notification
+
+
+class NotificationReadAdminView(BaseAdminView):
+	"""Update notifications as read."""
+	model = Notification
+
+	def init_request(self, *args, **kwargs):
+		self.url = self.get_model_url(self.model, "changelist")
+
+	def get(self, request, object_id, **kwargs):
+		obj = get_object_or_404(self.model, pk=object_id, recipient=self.user)
+
+		if not obj.read:
+			obj.is_read = True
+			obj.read_datetime = timezone.now()
+			obj.save()
+
+		return redirect(obj.url or self.url)
