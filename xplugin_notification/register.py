94c8221a238fec1084096126465c1e35051e3af0
alexsilva <geniofuturo@gmail.com>
2024-08-09 11:44:55 -0300

    Creating notification with slug field and updating existing one.

--- xplugin_notification/register.py
+++ xplugin_notification/register.py
@@ -15,22 +15,24 @@ class NotificationAdmin:
 	def __init__(self):
 		self.opts = self.notification_model._meta
 
-	def notify(self, recipient, message: str, source=None, **options):
+	def notify(self, recipient, message: str, source=None, slug=None, **options) -> Notification:
 		"""Notifies a users"""
 		if not (recipient.is_staff and recipient.is_active):
 			# only sends notifications to active staff and users.
 			return
-		obj = self.notification_model.objects.create(
+		defaults = dict(options)
+		defaults.update(message=message)
+		obj, created = self.notification_model.objects.update_or_create(
+			slug=slug,
 			recipient=recipient,
 			source=source,
-			message=message,
-			**options
+			defaults=defaults
 		)
 		# register permission for object.
 		for perm_name in self.permission_names:
 			permission_codename = get_permission_codename(perm_name, self.opts)
 			assign_perm(permission_codename, recipient, obj)
-		return notification
+		return obj
 
 	def notify_groups(self, groups: tuple, message: str, source=None, **options):
 		"""Notifies a list of user groups"""
